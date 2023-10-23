1ea00557bebee6e384d5cf0880339a52b07a1220
SantatraM <santatryn@gmail.com>
2023-10-23 03:16:23 +0300

    commit

--- GestionEntreprise/application/views/demandeConge.php
+++ GestionEntreprise/application/views/demandeConge.php
@@ -1,68 +1,62 @@
+<div class="content-wrapper">
+<div class="container-xxl flex-grow-1 container-p-y">
+              <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Demande de</span> Conge</h4>
 
-
-<div class="container-xxl flex-grow-1 container-p-y" style="margin-left:25%">
-              <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Demande conge</h4>
-
-              <!-- Basic Layout & Basic with Icons -->
+             <!-- Basic Layout -->
               <div class="row">
-                <!-- Basic Layout -->
-                <div class="col-xxl">
+                <div class="col-xl">
                   <div class="card mb-4">
+                    <div class="card-header d-flex justify-content-between align-items-center">
+                      <h5 class="mb-0">Formulaire de demande de conge</h5>
+                      <!-- <small class="text-muted float-end">Default label</small> -->
+                    </div>
                     <div class="card-body">
-                      <form>
-                        <div class="row mb-3">
-                          <label class="col-sm-2 col-form-label" for="basic-default-name">Matricule</label>
-                          <div class="col-sm-10">
-                            <!-- <input type="text" class="form-control" id="basic-default-name" placeholder="John Doe" /> -->
-                            <select name="iddemande" id="basic-default-phone" class="form-control phone-mask">
-                                  <option>matricule</option>
+                        <?php if(isset($erreur)){
+                            echo "<p style=color:red;>".$erreur."</p>";
+                        }?>
+                      <form action="<?php echo site_url("welcome/formulaireDemandeConge"); ?>" method="post">
+                        <div class="mb-3">
+                          <label class="form-label" for="basic-default-fullname">Matricule de l'Employe</label>
+                          <input type="text" class="form-control" id="basic-default-fullname" name="matricule" placeholder="Matricule" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="defaultSelect" class="form-label">type de Conge</label>
+                            <select id="defaultSelect" class="form-select">
+                            <option>Choisir</option>
+                            <?php for ($i=0; $i < count($typeconge); $i++) { ?>
+                                <option value="<?php echo $typeconge[$i]->idtypeconge; ?>"><?php echo $typeconge[$i]->libelle; ?></option>
+                            <?php } ?>
                             </select>
-                          </div>
                         </div>
-                        <div class="row mb-3">
-                          <label class="col-sm-2 col-form-label" for="basic-default-company">Type</label>
-                          <div class="col-sm-10">
-                            <select name="typeconge" id="basic-default-phone" class="form-control phone-mask">
-                                  <option>typeConge</option>
-                                  <?php foreach($typeConge as $typeConges) { ?>
-                                  <option value="<?php echo $typeConges['idtypeconge'] ?>"><?php echo $typeConges['libelle']; ?></option>
-                                  <?php } ?>
-                            </select>
-                          </div>
-                        </div>
-                        <div class="row mb-3">
-                          <label class="col-sm-2 col-form-label" for="basic-default-email">Debut</label>
-                          <div class="col-sm-10">
-                            <div class="input-group input-group-merge">
-                              <input
-                                    type="date"
-                                  id="basic-default-phone"
-                                  class="form-control phone-mask"
-                                  name="debut"
-                              />
-                            </div>
-                          </div>
-                        </div>
-                        <div class="row mb-3">
-                          <label class="col-sm-2 col-form-label" for="basic-default-phone">Fin</label>
-                          <div class="col-sm-10">
+                        <div class="mb-3">
+                        <label class="form-label" for="basic-default-company" name="datedebut">Date debut du Conge</label>
+                            <!-- <div class="col-md-12"> -->
                             <input
-                                    type="date"
-                                  id="basic-default-phone"
-                                  class="form-control phone-mask"
-                                  name="fin"
+                                class="form-control"
+                                type="datetime-local"
+                                id="html5-datetime-local-input"
+                                name="datedebut"
                             />
-                          </div>
+                                <!-- </div> -->
+                            <!-- <div class="form-text">You can use letters, numbers & periods</div> -->
                         </div>
-                        <div class="row justify-content-end">
-                          <div class="col-sm-10">
-                            <button type="submit" class="btn btn-primary">Send</button>
-                          </div>
+                        <div class="mb-3">
+                        <label class="form-label" for="basic-default-company" name="datefin">Date fin du Conge</label>
+                            <!-- <div class="col-md-12"> -->
+                            <input
+                                class="form-control"
+                                type="datetime-local"
+                                id="html5-datetime-local-input"
+                                name="datefin"
+                            />
+                                <!-- </div> -->
+                            <!-- <div class="form-text">You can use letters, numbers & periods</div> -->
                         </div>
+                        <button type="submit" class="btn btn-primary">Soumetre</button>
                       </form>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <!-- / Content -->
+</div>
+</div>
